346f31481a2dd6646dd310a7dcbcc63b5887a770
Alexis Villagran <alexisvillagran18@hotmail.com>
2025-02-16 21:38:40 -0700

    added all da code

--- AccumulateMessages/MessageProgram.vb
+++ AccumulateMessages/MessageProgram.vb
@@ -1,5 +1,13 @@
+'Alexis Villagran
+'RCET0265
+'Spring 2025
+'ConvertAndValidate
+'
 
-Imports System
+Option Explicit On
+Option Strict On
+Option Compare Binary
+
 
 Module MessageProgram
     Sub Main(args As String())
@@ -9,8 +17,17 @@ Module MessageProgram
     End Sub
 
     Function UserMessages(ByVal newMessage As String, ByVal clear As Boolean) As String
-        'your code here
-        Return messages
+        'your code her
+        Static text As New Text.StringBuilder
+        If clear Then
+            text.clear()
+        ElseIf Not String.IsNullOrEmpty(newMessage) Then
+            text.AppendLine(newMessage)
+        End If
+
+
+
+        Return text.ToString
     End Function
 
 
